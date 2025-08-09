c76e81bc2faa6bf5e6997a7583253643ad454112
Abhik Roychoudhury <abhik@comp.nus.edu.sg>
2025-08-09 12:09:19 +0800

    Update ResearchSection.tsx

--- src/components/ResearchSection.tsx
+++ src/components/ResearchSection.tsx
@@ -22,7 +22,7 @@ const researchProjects = [
    {
     title: "Unified Software Engineering Agent",
     description: "An agent which can write, fix, patch code and conduct many SE tasks",
-    link: "#useagent",
+    link: "https://arxiv.org/abs/2506.14683",
   },
   { title: "Automatic Programming",
     description: "Position paper in TOSEM",
