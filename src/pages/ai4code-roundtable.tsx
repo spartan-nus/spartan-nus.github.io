f6d8c98d2e3cae9df5dc065dd0c4902e5a2ece12
imamnurby <imamnurby@gmail.com>
2025-09-04 13:49:00 +0800

    fix broken marker

--- src/pages/ai4code-roundtable.tsx
+++ src/pages/ai4code-roundtable.tsx
@@ -4,6 +4,14 @@ import React, { useState, useEffect, useRef } from "react";
 import Navbar from "@/components/Navbar";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 
 // --- START: New and Improved SectionNavbar Logic ---
