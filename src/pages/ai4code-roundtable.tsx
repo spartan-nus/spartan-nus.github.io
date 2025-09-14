525ba2a49241ad4a6cf64bee1e508af3c394d889
Abhik Roychoudhury <abhik@comp.nus.edu.sg>
2025-09-14 17:36:41 +0800

    Update ai4code-roundtable.tsx

--- src/pages/ai4code-roundtable.tsx
+++ src/pages/ai4code-roundtable.tsx
@@ -482,14 +482,14 @@ const YourNewPage = () => {
                 <div className="flex border-l-4 border-blue-500 pl-4 py-2">
                   <div className="w-20 text-sm font-semibold text-gray-500">10:00 AM</div>
                   <div className="flex-1">
-                    <h3 className="font-semibold">Industry Sharing 1: Tech company / govt agency from Singapore (to fix)</h3>
+                    <h3 className="font-semibold">Industry Sharing: Overarching Talk on Trust</h3>
                     {/* <p className="text-gray-600">Networking and Discussion</p> */}
                   </div>
                 </div>
                 <div className="flex border-l-4 border-blue-500 pl-4 py-2">
-                  <div className="w-20 text-sm font-semibold text-gray-500">11:00 AM</div>
+                  <div className="w-20 text-sm font-semibold text-gray-500">10:30  AM</div>
                   <div className="flex-1">
-                    <h3 className="font-semibold">Industry sharing 2: Tech company  from USA / academic industrial researcher from USA (to fix)</h3>
+                    <h3 className="font-semibold">Industry sharing: Talks from industry / govt agency from Singapore / USA (3-4 talks) </h3>
                     {/* <p className="text-gray-600">Hands-on Interpretability Tools</p> */}
                   </div>
                 </div>
